c63a07cc5fa68d43ec5b2ea4e97a6f3e961966d9
Yusuf Tuncay <yusuf@tuncay.be>
2024-03-14 14:54:32 +0100

    Add project files.

---
+++ Forms/MainForm.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace QuickerChat.Forms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GroupBoxPresets = new System.Windows.Forms.GroupBox();
+            this.RadioButtonCustomPreset = new System.Windows.Forms.RadioButton();
+            this.RadioButtonPreset1 = new System.Windows.Forms.RadioButton();
+            this.RadioButtonPreset2 = new System.Windows.Forms.RadioButton();
+            this.RadioButtonPreset3 = new System.Windows.Forms.RadioButton();
+            this.RadioButtonPreset4 = new System.Windows.Forms.RadioButton();
+            this.TextBoxCustom = new System.Windows.Forms.TextBox();
+            this.GroupBoxActions = new System.Windows.Forms.GroupBox();
+            this.ButtonChangeKeybind = new System.Windows.Forms.Button();
+            this.ButtonCheckController = new System.Windows.Forms.Button();
+            this.ButtonReset = new System.Windows.Forms.Button();
+            this.LabelController = new System.Windows.Forms.Label();
+            this.MenuStripMainPage = new System.Windows.Forms.MenuStrip();
+            this.FileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SocialsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EmailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GithubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.GroupBoxPresets.SuspendLayout();
+            this.GroupBoxActions.SuspendLayout();
+            this.MenuStripMainPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBoxPresets
+            // 
+            this.GroupBoxPresets.Controls.Add(this.RadioButtonCustomPreset);
+            this.GroupBoxPresets.Controls.Add(this.RadioButtonPreset1);
+            this.GroupBoxPresets.Controls.Add(this.RadioButtonPreset2);
+            this.GroupBoxPresets.Controls.Add(this.RadioButtonPreset3);
+            this.GroupBoxPresets.Controls.Add(this.RadioButtonPreset4);
+            this.GroupBoxPresets.Controls.Add(this.TextBoxCustom);
+            this.GroupBoxPresets.Location = new System.Drawing.Point(13, 27);
+            this.GroupBoxPresets.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.GroupBoxPresets.Name = "GroupBoxPresets";
+            this.GroupBoxPresets.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.GroupBoxPresets.Size = new System.Drawing.Size(308, 220);
+            this.GroupBoxPresets.TabIndex = 0;
+            this.GroupBoxPresets.TabStop = false;
+            this.GroupBoxPresets.Text = "Presets";
+            // 
+            // RadioButtonCustomPreset
+            // 
+            this.RadioButtonCustomPreset.AutoSize = true;
+            this.RadioButtonCustomPreset.Location = new System.Drawing.Point(8, 122);
+            this.RadioButtonCustomPreset.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.RadioButtonCustomPreset.Name = "RadioButtonCustomPreset";
+            this.RadioButtonCustomPreset.Size = new System.Drawing.Size(67, 19);
+            this.RadioButtonCustomPreset.TabIndex = 4;
+            this.RadioButtonCustomPreset.Text = "Custom";
+            this.RadioButtonCustomPreset.UseVisualStyleBackColor = true;
+            this.RadioButtonCustomPreset.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // RadioButtonPreset1
+            // 
+            this.RadioButtonPreset1.AutoSize = true;
+            this.RadioButtonPreset1.Checked = true;
+            this.RadioButtonPreset1.Location = new System.Drawing.Point(8, 22);
+            this.RadioButtonPreset1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.RadioButtonPreset1.Name = "RadioButtonPreset1";
+            this.RadioButtonPreset1.Size = new System.Drawing.Size(110, 19);
+            this.RadioButtonPreset1.TabIndex = 0;
+            this.RadioButtonPreset1.TabStop = true;
+            this.RadioButtonPreset1.Text = "What a kkr save!";
+            this.RadioButtonPreset1.UseVisualStyleBackColor = true;
+            this.RadioButtonPreset1.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // RadioButtonPreset2
+            // 
+            this.RadioButtonPreset2.AutoSize = true;
+            this.RadioButtonPreset2.Location = new System.Drawing.Point(8, 47);
+            this.RadioButtonPreset2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.RadioButtonPreset2.Name = "RadioButtonPreset2";
+            this.RadioButtonPreset2.Size = new System.Drawing.Size(81, 19);
+            this.RadioButtonPreset2.TabIndex = 1;
+            this.RadioButtonPreset2.Text = "Kkr noobs!";
+            this.RadioButtonPreset2.UseVisualStyleBackColor = true;
+            this.RadioButtonPreset2.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // RadioButtonPreset3
+            // 
+            this.RadioButtonPreset3.AutoSize = true;
+            this.RadioButtonPreset3.Location = new System.Drawing.Point(8, 72);
+            this.RadioButtonPreset3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.RadioButtonPreset3.Name = "RadioButtonPreset3";
+            this.RadioButtonPreset3.Size = new System.Drawing.Size(117, 19);
+            this.RadioButtonPreset3.TabIndex = 2;
+            this.RadioButtonPreset3.Text = "Easiest shot ever..";
+            this.RadioButtonPreset3.UseVisualStyleBackColor = true;
+            this.RadioButtonPreset3.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // RadioButtonPreset4
+            // 
+            this.RadioButtonPreset4.AutoSize = true;
+            this.RadioButtonPreset4.Location = new System.Drawing.Point(8, 97);
+            this.RadioButtonPreset4.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.RadioButtonPreset4.Name = "RadioButtonPreset4";
+            this.RadioButtonPreset4.Size = new System.Drawing.Size(56, 19);
+            this.RadioButtonPreset4.TabIndex = 3;
+            this.RadioButtonPreset4.Text = "Bruh..";
+            this.RadioButtonPreset4.UseVisualStyleBackColor = true;
+            this.RadioButtonPreset4.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
+            // 
+            // TextBoxCustom
+            // 
+            this.TextBoxCustom.Enabled = false;
+            this.TextBoxCustom.Location = new System.Drawing.Point(8, 147);
+            this.TextBoxCustom.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.TextBoxCustom.Multiline = true;
+            this.TextBoxCustom.Name = "TextBoxCustom";
+            this.TextBoxCustom.Size = new System.Drawing.Size(292, 67);
+            this.TextBoxCustom.TabIndex = 5;
+            this.TextBoxCustom.TextChanged += new System.EventHandler(this.TextBoxCustom_TextChanged);
+            // 
+            // GroupBoxActions
+            // 
+            this.GroupBoxActions.Controls.Add(this.ButtonChangeKeybind);
+            this.GroupBoxActions.Controls.Add(this.ButtonCheckController);
+            this.GroupBoxActions.Controls.Add(this.ButtonReset);
+            this.GroupBoxActions.Controls.Add(this.LabelController);
+            this.GroupBoxActions.Location = new System.Drawing.Point(13, 253);
+            this.GroupBoxActions.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.GroupBoxActions.Name = "GroupBoxActions";
+            this.GroupBoxActions.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.GroupBoxActions.Size = new System.Drawing.Size(308, 196);
+            this.GroupBoxActions.TabIndex = 1;
+            this.GroupBoxActions.TabStop = false;
+            this.GroupBoxActions.Text = "Actions";
+            // 
+            // ButtonChangeKeybind
+            // 
+            this.ButtonChangeKeybind.Location = new System.Drawing.Point(7, 130);
+            this.ButtonChangeKeybind.Name = "ButtonChangeKeybind";
+            this.ButtonChangeKeybind.Size = new System.Drawing.Size(294, 27);
+            this.ButtonChangeKeybind.TabIndex = 5;
+            this.ButtonChangeKeybind.Text = "Change Keybind";
+            this.ButtonChangeKeybind.UseVisualStyleBackColor = true;
+            this.ButtonChangeKeybind.Click += new System.EventHandler(this.ButtonChangeKeybind_Click);
+            // 
+            // ButtonCheckController
+            // 
+            this.ButtonCheckController.Location = new System.Drawing.Point(8, 95);
+            this.ButtonCheckController.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ButtonCheckController.Name = "ButtonCheckController";
+            this.ButtonCheckController.Size = new System.Drawing.Size(292, 27);
+            this.ButtonCheckController.TabIndex = 3;
+            this.ButtonCheckController.Text = "Search Controller";
+            this.ButtonCheckController.UseVisualStyleBackColor = true;
+            this.ButtonCheckController.Click += new System.EventHandler(this.ButtonCheckController_Click);
+            // 
+            // ButtonReset
+            // 
+            this.ButtonReset.Location = new System.Drawing.Point(7, 163);
+            this.ButtonReset.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ButtonReset.Name = "ButtonReset";
+            this.ButtonReset.Size = new System.Drawing.Size(294, 27);
+            this.ButtonReset.TabIndex = 2;
+            this.ButtonReset.Text = "Reset";
+            this.ButtonReset.UseVisualStyleBackColor = true;
+            this.ButtonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
+            // LabelController
+            // 
+            this.LabelController.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.LabelController.ForeColor = System.Drawing.Color.Black;
+            this.LabelController.Location = new System.Drawing.Point(8, 19);
+            this.LabelController.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LabelController.Name = "LabelController";
+            this.LabelController.Size = new System.Drawing.Size(292, 62);
+            this.LabelController.TabIndex = 4;
+            this.LabelController.Text = "...";
+            this.LabelController.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MenuStripMainPage
+            // 
+            this.MenuStripMainPage.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.FileToolStripMenuItem,
+            this.SocialsToolStripMenuItem});
+            this.MenuStripMainPage.Location = new System.Drawing.Point(0, 0);
+            this.MenuStripMainPage.Name = "MenuStripMainPage";
+            this.MenuStripMainPage.Size = new System.Drawing.Size(334, 24);
+            this.MenuStripMainPage.TabIndex = 2;
+            this.MenuStripMainPage.Text = "MenuStripMainPage";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.FileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
+            this.FileToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.FileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.FileToolStripMenuItem.Text = "File";
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.ExitToolStripMenuItem.Text = "Exit";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // SocialsToolStripMenuItem
+            // 
+            this.SocialsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.GithubToolStripMenuItem,
+            this.EmailToolStripMenuItem});
+            this.SocialsToolStripMenuItem.Name = "SocialsToolStripMenuItem";
+            this.SocialsToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.SocialsToolStripMenuItem.Text = "Socials";
+            // 
+            // EmailToolStripMenuItem
+            // 
+            this.EmailToolStripMenuItem.Name = "EmailToolStripMenuItem";
+            this.EmailToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.EmailToolStripMenuItem.Text = "Email";
+            this.EmailToolStripMenuItem.Click += new System.EventHandler(this.EmailToolStripMenuItem_Click);
+            // 
+            // GithubToolStripMenuItem
+            // 
+            this.GithubToolStripMenuItem.Name = "GithubToolStripMenuItem";
+            this.GithubToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.GithubToolStripMenuItem.Text = "Github";
+            this.GithubToolStripMenuItem.Click += new System.EventHandler(this.GithubToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 461);
+            this.Controls.Add(this.GroupBoxActions);
+            this.Controls.Add(this.GroupBoxPresets);
+            this.Controls.Add(this.MenuStripMainPage);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.75F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "QuickerChat";
+            this.GroupBoxPresets.ResumeLayout(false);
+            this.GroupBoxPresets.PerformLayout();
+            this.GroupBoxActions.ResumeLayout(false);
+            this.MenuStripMainPage.ResumeLayout(false);
+            this.MenuStripMainPage.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Button ButtonChangeKeybind;
+        private System.Windows.Forms.Button ButtonCheckController;
+        private System.Windows.Forms.Button ButtonReset;
+        private System.Windows.Forms.ToolStripMenuItem EmailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem FileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem GithubToolStripMenuItem;
+        private System.Windows.Forms.GroupBox GroupBoxActions;
+        private System.Windows.Forms.GroupBox GroupBoxPresets;
+        private System.Windows.Forms.Label LabelController;
+        private System.Windows.Forms.MenuStrip MenuStripMainPage;
+        private System.Windows.Forms.RadioButton RadioButtonCustomPreset;
+        private System.Windows.Forms.RadioButton RadioButtonPreset1;
+        private System.Windows.Forms.RadioButton RadioButtonPreset2;
+        private System.Windows.Forms.RadioButton RadioButtonPreset3;
+        private System.Windows.Forms.RadioButton RadioButtonPreset4;
+        private System.Windows.Forms.ToolStripMenuItem SocialsToolStripMenuItem;
+        private System.Windows.Forms.TextBox TextBoxCustom;
+    }
+}
---
+++ Forms/MainForm.cs
@@ -0,0 +1,245 @@
+﻿#region Using
+using SharpDX.DirectInput;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Threading;
+using System.Windows.Forms;
+#endregion
+
+namespace QuickerChat.Forms
+{
+    public partial class MainForm : Form
+    {
+        #region Variables
+        /// <summary>
+        /// Initializes the program by setting up a background worker to read _joystick polls in the background
+        /// </summary>
+        private BackgroundWorker _backgroundWorker;
+
+        /// <summary>
+        /// Represents a _joystick device for input control
+        /// </summary>
+        private Joystick _joystick;
+
+        /// <summary>
+        /// Controls the loop of the background worker, allowing for starting and stopping
+        /// </summary>
+        private bool _isBackgroundWorkerLoop;
+
+        /// <summary>
+        /// Form for changing keybindings in the program
+        /// </summary>
+        //private static readonly ChangeKeybindForm changeKeybindForm = new ChangeKeybindForm();
+
+        private string _textToSpam;
+        #endregion
+
+        #region MainForm
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeController();
+        }
+        #endregion
+
+        #region Import DLL
+        [DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        #endregion
+
+        #region Controller Methods
+        private void InitializeController()
+        {
+            LabelController.Text = "";
+            LabelController.ForeColor = Color.Yellow;
+
+            try
+            {
+                using (var directInput = new DirectInput())
+                {
+                    // Get First Joystick
+                    foreach (var deviceInstance in directInput.GetDevices(DeviceClass.Keyboard, DeviceEnumerationFlags.AllDevices))
+                    {
+                        _joystick = new Joystick(directInput, deviceInstance.InstanceGuid);
+                        break;
+                    }
+                }
+
+                if (_joystick != null)
+                {
+                    LabelController.Text = $"Controller Connected\n{_joystick}";
+                    LabelController.ForeColor = Color.Green;
+
+                    // Enable Presets and BgWorker Loop
+                    GroupBoxPresets.Enabled = true;
+                    _isBackgroundWorkerLoop = true;
+                } else
+                {
+                    LabelController.Text = "No Controller Connected";
+                    LabelController.ForeColor = Color.Red;
+
+                    // Disable Presets and BgWorker Loop
+                    GroupBoxPresets.Enabled = false;
+                    _isBackgroundWorkerLoop = false;
+                    return;
+                }
+
+                // Acquire the _joystick
+                _joystick.Properties.BufferSize = 128;
+                _joystick.Acquire();
+
+                // Start a background worker to continuously poll the controller
+                _backgroundWorker = new BackgroundWorker();
+                _backgroundWorker.DoWork += BackgroundWorker_DoWork;
+                _backgroundWorker.RunWorkerAsync();
+            } catch (Exception) { }
+        }
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (_isBackgroundWorkerLoop)
+            {
+                _joystick.Poll();
+                JoystickState data = _joystick.GetCurrentState();
+
+                // Check button presses
+                bool circleButtonPressed = data.Buttons[2]; // Circle
+
+                if (circleButtonPressed)
+                {
+                    StartSpam();
+                    Thread.Sleep(200);
+                }
+
+                Thread.Sleep(10);
+            }
+        }
+        #endregion
+
+        #region Spam Methods
+
+        private void StartSpam()
+        {
+            try
+            {
+                // Find the Rocket League process
+                var process = Process.GetProcessesByName("RocketLeague").FirstOrDefault();
+
+                if (process == null)
+                    return;
+
+                // Wait
+                process.WaitForInputIdle();
+
+                // Focus on Rocket League
+                SetForegroundWindow(process.MainWindowHandle);
+
+                // Spam 3 times
+                for (var i = 0; i < 3; i++)
+                    SendKeys.SendWait($"T{_textToSpam}{{ENTER}}");
+
+            } catch (Exception) { }
+        }
+        #endregion
+
+        #region "Click" Events
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            CleanStates();
+        }
+        private void ButtonCheckController_Click(object sender, EventArgs e)
+        {
+            InitializeController();
+        }
+        private void ButtonChangeKeybind_Click(object sender, EventArgs e)
+        {
+            //changeKeybindForm.Show();
+        }
+        #endregion
+
+        #region "Changed" Events
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // Check for custom input
+            if (RadioButtonCustomPreset.Checked)
+            {
+                // Set "_textToSpam" property
+                _textToSpam = null;
+                TextBoxCustom.Enabled = true;
+            } else
+            {
+                // Set "_textToSpam" property
+                _textToSpam = ((RadioButton)sender).Text;
+                TextBoxCustom.Enabled = false;
+            }
+        }
+        private void TextBoxCustom_TextChanged(object sender, EventArgs e)
+        {
+            // Set "_textToSpam" property
+            _textToSpam = TextBoxCustom.Text;
+        }
+        #endregion
+
+        #region Dispose
+        /// <summary>
+        /// Dispose Managed Resources
+        /// </summary>
+        private void CleanStates()
+        {
+            // BgWorker
+            if (_backgroundWorker != null)
+            {
+                // Stop Loop
+                _isBackgroundWorkerLoop = false;
+
+                _backgroundWorker.DoWork -= BackgroundWorker_DoWork;
+                _backgroundWorker.Dispose();
+                _backgroundWorker = null;
+            }
+
+            // Joystick
+            if (_joystick != null)
+            {
+                _joystick.Dispose();
+                _joystick = null;
+            }
+
+            // Label
+            LabelController.Text = "No Controller Connected";
+            LabelController.ForeColor = Color.Red;
+
+            // RadioButton
+            RadioButtonPreset1.Checked = true;
+        }
+        #endregion
+
+        #region MenuStrip MainPage
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Dispose(true);
+            Close();
+        }
+        private void GithubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/yusuftuncay");
+        }
+        private void EmailToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string recipient = "yusuf@tuncay.be";
+                string subject = "QuickerChat Question";
+                string mailtoUri = $"mailto:{recipient}?subject={Uri.EscapeDataString(subject)}";
+                Process.Start(mailtoUri);
+            } catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+    }
+}
